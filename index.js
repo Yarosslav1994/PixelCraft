d876c41b822eabc47c14a389f65767817a92cd9c
Ivan Petrenko <107744427+Yarosslav1994@users.noreply.github.com>
2025-11-02 11:24:42 +0200

    Move HTML to public and add routes

--- index.js
+++ index.js
@@ -1,31 +1,58 @@
+// Підключаємо необхідні модулі
 const express = require('express');
 const path = require('path');
 
 const app = express();
+
+// Порт від Render або 3000 для локального запуску
 const PORT = process.env.PORT || 3000;
 
-// Дозволяємо доступ до всіх статичних файлів у public
+// Вказуємо папку зі статичними файлами (CSS, JS, картинки, відео)
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Головна сторінка
+// Маршрут для головної сторінки
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Сторінка логіну
+// Маршрут для сторінки логіну
 app.get('/login', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'login.html'));
 });
 
-// Сторінка реєстрації
+// Маршрут для сторінки реєстрації
 app.get('/register', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'register.html'));
 });
 
-// Можна додати інші HTML файли так само
-// app.get('/course_blocks', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'public', 'course_blocks.html'));
-// });
+// Маршрути для всіх модулів та course_blocks
+app.get('/course_blocks.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'course_blocks.html'));
+});
+app.get('/Module1.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Module1.html'));
+});
+app.get('/Module2.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Module2.html'));
+});
+app.get('/Module3.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Module3.html'));
+});
+app.get('/Module4.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Module4.html'));
+});
+app.get('/Module5.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Module5.html'));
+});
+app.get('/Module6.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Module6.html'));
+});
+app.get('/Module7.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Module7.html'));
+});
+app.get('/Module8.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Module8.html'));
+});
 
 // Запускаємо сервер
 app.listen(PORT, () => {
